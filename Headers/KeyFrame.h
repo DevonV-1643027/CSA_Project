80a380ce7aea1a681f9e9fbf55c710f6f3e82cff
DevonV-1643027 <devon.vanaenrode@student.uhasselt.be>
2024-08-06 11:17:35 +0200

    Update KeyFrame.h
    Create a data structure to store FFD control points.
    Extend the KeyFrame class to include a list of FFD control points.

--- Headers/KeyFrame.h
+++ Headers/KeyFrame.h
@@ -7,12 +7,25 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtx/quaternion.hpp>
+#include <vector>
 
+// Define the FFDControlPoint structure
+struct FFDControlPoint {
+    glm::vec3 position;        // Current position of the control point
+    glm::vec3 originalPosition; // Original position of the control point
+    float weight;              // Influence weight of the control point
+
+    FFDControlPoint(glm::vec3 pos, glm::vec3 origPos, float w)
+        : position(pos), originalPosition(origPos), weight(w) {}
+};
+
+// Define the KeyFrame structure
 struct KeyFrame {
     float timestamp;          // Time at which this key frame occurs
     glm::vec3 position;       // Position of the object at this key frame
     glm::quat rotation;       // Rotation of the object at this key frame
     glm::vec3 scale;          // Scale of the object at this key frame
+    std::vector<FFDControlPoint> ffdControlPoints; // List of FFD control points
 
     KeyFrame(float time, glm::vec3 pos, glm::quat rot, glm::vec3 scl)
         : timestamp(time), position(pos), rotation(rot), scale(scl) {}
